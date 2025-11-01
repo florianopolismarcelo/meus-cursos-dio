a6665f020d6163ca49fddbaf902ad22435d65b9c
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-11-01 15:51:43 -0300

    javascript do jogo da velha

--- orbi-web-games-developer/004 - jogo da velha/assets/js/scripts.js
+++ orbi-web-games-developer/004 - jogo da velha/assets/js/scripts.js
@@ -1,5 +1,6 @@
 var jogador = null
 var jogadorSelecionado = document.getElementById("jogador-selecionado")
+var quadrados = document.getElementsByClassName("quadrado")
 
 mudarJogador("X")
 
@@ -23,4 +24,16 @@ function escolherQuadrado(id) {
 function mudarJogador(valor) {
   jogador = valor
   jogadorSelecionado.innerHTML = jogador
+}
+
+function checarVencedor() {
+  var quadrado1 = document.getElementById(1)
+  var quadrado2 = document.getElementById(2)
+  var quadrado3 = document.getElementById(3)
+  var quadrado4 = document.getElementById(4)
+  var quadrado5 = document.getElementById(5)
+  var quadrado6 = document.getElementById(6)
+  var quadrado7 = document.getElementById(7)
+  var quadrado8 = document.getElementById(8)
+  var quadrado9 = document.getElementById(9)
 }
