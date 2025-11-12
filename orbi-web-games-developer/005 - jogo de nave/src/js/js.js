1df8b4fbf8aef4c8ee0f092c5ca045cb243baace
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-11-12 15:57:10 -0300

    movimentação inimigo2

--- orbi-web-games-developer/005 - jogo de nave/src/js/js.js
+++ orbi-web-games-developer/005 - jogo de nave/src/js/js.js
@@ -32,6 +32,7 @@ function start() {
     movefundo()
     movejogador()
     moveinimigo1()
+    moveinimigo2()
   } // Fim da função loop()
 
   // função que movimenta o fundo do jogo
@@ -76,5 +77,13 @@ function start() {
       $("#inimigo1").css("top", posicaoY)
     }
   } // Fim da função moveinimigo1()
+
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"))
+    $("#inimigo2").css("left", posicaoX - 3)
+    if (posicaoX <= 0) {
+      $("#inimigo2").css("left", 775)
+    }
+  } // Fim da função moveinimigo2()
   
 } // Fim da função start()
