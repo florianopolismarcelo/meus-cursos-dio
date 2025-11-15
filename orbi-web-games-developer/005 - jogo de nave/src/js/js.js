8b6793db7fcc4b89fc13a5c5cef0200d7b9972c5
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-11-15 07:52:28 -0300

    disparo helicóptero

--- orbi-web-games-developer/005 - jogo de nave/src/js/js.js
+++ orbi-web-games-developer/005 - jogo de nave/src/js/js.js
@@ -7,6 +7,7 @@ function start() {
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>")
 
   //Principais variáveis do jogo
+  var podeAtirar = true
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334)
@@ -64,6 +65,7 @@ function start() {
 
     if (jogo.pressionou[TECLA.D]) {
       //Chama a função Disparo
+      disparo()
     }
   } // Fim da função movejogador()
 
@@ -95,4 +97,33 @@ function start() {
     }
   } // Fim da função moveamigo()
 
+  // função que realiza o disparo da arma do helicóptero cinza
+  function disparo() {
+    if (podeAtirar == true) {
+      podeAtirar = false
+
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX = parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190
+      topoTiro = topo + 37
+      $("#fundoGame").append("<div id='disparo'></div")
+      $("#disparo").css("top", topoTiro)
+      $("#disparo").css("left", tiroX)
+
+      var tempoDisparo = window.setInterval(executaDisparo, 5) // velocidade do disparo
+    }
+
+    // função que realiza o disparo da arma
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"))
+      $("#disparo").css("left", posicaoX + 25) // velocidade do disparo
+
+      if (posicaoX > 900) {
+        window.clearInterval(tempoDisparo)
+        tempoDisparo = null
+        $("#disparo").remove()
+        podeAtirar = true
+      }
+    }
+  }
 } // Fim da função start()
