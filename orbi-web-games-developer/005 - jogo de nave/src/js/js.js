18f6882d5d54761f805392fe20a3f758c8bde0e4
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-11-19 17:52:24 -0300

    reposiciona inimigo 2 e explosão inimigo 2

--- orbi-web-games-developer/005 - jogo de nave/src/js/js.js
+++ orbi-web-games-developer/005 - jogo de nave/src/js/js.js
@@ -8,6 +8,7 @@ function start() {
 
   //Principais variáveis do jogo
   var podeAtirar = true
+  var fimdejogo = false
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334)
@@ -130,6 +131,11 @@ function start() {
 
   function colisao() {
     var colisao1 = $("#jogador").collision($("#inimigo1"))
+    var colisao2 = $("#jogador").collision($("#inimigo2"))
+    var colisao3 = $("#disparo").collision($("#inimigo1"))
+    var colisao4 = $("#disparo").collision($("#inimigo2"))
+    var colisao5 = $("#jogador").collision($("#amigo"))
+    var colisao6 = $("#inimigo2").collision($("#amigo"))
     // jogador com inimigo1
     if (colisao1.length > 0) {
       inimigo1X = parseInt($("#inimigo1").css("left"))
@@ -139,6 +145,16 @@ function start() {
       posicaoY = parseInt(Math.random() * 334)
       $("#inimigo1").css("left", 694)
       $("#inimigo1").css("top", posicaoY)
+    }
+
+    // colisão com o inimigo2, caminhão
+    if (colisao2.length > 0) {
+      inimigo2X = parseInt($("#inimigo2").css("left"))
+      inimigo2Y = parseInt($("#inimigo2").css("top"))
+      explosao2(inimigo2X, inimigo2Y)
+
+      $("#inimigo2").remove()
+      reposicionaInimigo2()
     }
   } // Fim da função colisao()
 
@@ -163,4 +179,40 @@ function start() {
       tempoExplosao = null
     }
   } // Fim da função explosao1()
+
+  // função que reposiciona inimigo2, caminhão
+  function reposicionaInimigo2() {
+    var tempoColisao4 = window.setInterval(reposiciona4, 5000)
+
+    function reposiciona4() {
+      window.clearInterval(tempoColisao4)
+      tempoColisao4 = null
+
+      if (fimdejogo == false) {
+        $("#fundoGame").append("<div id=inimigo2></div")
+      }
+    }
+  } // Fim da função reposicionaInimigo2()
+
+  // função da explosão, colisão com o inimigo 2, caminhão
+  function explosao2(inimigo2X, inimigo2Y) {
+    $("#fundoGame").append("<div id='explosao2'></div")
+    $("#explosao2").css(
+      "background-image",
+      "url(./src/assets/images/explosao.png)"
+    )
+
+    var div2 = $("#explosao2")
+    div2.css("top", inimigo2Y)
+    div2.css("left", inimigo2X)
+    div2.animate({ width: 200, opacity: 0 }, "slow")
+
+    var tempoExplosao2 = window.setInterval(removeExplosao2, 1000)
+
+    function removeExplosao2() {
+      div2.remove()
+      window.clearInterval(tempoExplosao2)
+      tempoExplosao2 = null
+    }
+  } // Fim da função explosao2()
 } // Fim da função start()
