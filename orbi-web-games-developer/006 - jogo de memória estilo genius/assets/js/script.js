dc716f5b3b9526bf484030a44bea8320aa3f6499
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-02 11:52:45 -0300

    javascript variáves

---
+++ orbi-web-games-developer/006 - jogo de memória estilo genius/assets/js/script.js
@@ -0,0 +1,13 @@
+let order = []
+let clickedOrder = []
+let score = 0
+
+//0 - verde
+//01 - vermelho
+//02 - amarelo
+//03 - azul
+
+const blue = document.querySelector(".blue")
+const red = document.querySelector(".red")
+const green = document.querySelector(".green")
+const yellow = document.querySelector(".yellow")
