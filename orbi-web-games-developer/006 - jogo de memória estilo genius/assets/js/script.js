6f1026ee452c0dbf70592f4993f8cd125e8f4146
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-03 16:21:37 -0300

    função shuffleorder

--- orbi-web-games-developer/006 - jogo de memória estilo genius/assets/js/script.js
+++ orbi-web-games-developer/006 - jogo de memória estilo genius/assets/js/script.js
@@ -11,3 +11,15 @@ const blue = document.querySelector(".blue")
 const red = document.querySelector(".red")
 const green = document.querySelector(".green")
 const yellow = document.querySelector(".yellow")
+
+//cria ordem aleatória de cores
+let shuffleOrder = () => {
+  let colorOrder = Math.floor(Math.random() * 4)
+  order[order.length] = colorOrder
+  clickedOrder = []
+
+  for (let i in order) {
+    let elementColor = createColorElement(order[i])
+    lightColor(elementColor, Number(i) + 1)
+  }
+}
