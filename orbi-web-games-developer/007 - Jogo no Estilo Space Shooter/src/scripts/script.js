0bbe73be68a3f03e57d41344e3f46216f222b4ad
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-12 19:48:02 -0300

    função subir

--- orbi-web-games-developer/007 - Jogo no Estilo Space Shooter/src/scripts/script.js
+++ orbi-web-games-developer/007 - Jogo no Estilo Space Shooter/src/scripts/script.js
@@ -14,3 +14,15 @@ function flyShip(event) {
     fireLaser();
   }
 }
+
+// função de subir
+function moveUp() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue("top");
+  if (topPosition === "0px") {
+    return;
+  } else {
+    let position = parseInt(topPosition);
+    position -= 50;
+    yourShip.style.top = `${position}px`;
+  }
+}
