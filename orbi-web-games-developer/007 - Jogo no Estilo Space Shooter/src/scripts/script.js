f0a948636aa42cd2315f39d229d979f025f85184
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-15 07:14:25 -0300

    função de movimentação do tiro

--- orbi-web-games-developer/007 - Jogo no Estilo Space Shooter/src/scripts/script.js
+++ orbi-web-games-developer/007 - Jogo no Estilo Space Shooter/src/scripts/script.js
@@ -60,3 +60,26 @@ function createLaserElement() {
   newLaser.style.top = `${yPosition - 10}px`;
   return newLaser;
 }
+
+// função de movimentação do tiro
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+        let aliens = document.querySelectorAll('.alien');
+
+        aliens.forEach((alien) => { //comparando se cada alien foi atingido, se sim, troca o src da imagem
+            if(checkLaserCollision(laser, alien)) {
+                alien.src = 'img/explosion.png';
+                alien.classList.remove('alien');
+                alien.classList.add('dead-alien');
+            }
+        })
+
+        if(xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`;
+        }
+    }, 10);
+}
+
