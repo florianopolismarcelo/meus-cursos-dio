5b3d0a417ce2b8c8ac485e13b6e54fd4111cce45
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-22 15:54:38 -0300

    função tecla que irá precionar

---
+++ orbi-web-games-developer/008 - jogo do dinossauro sem internet/assets/js/script.js
@@ -0,0 +1,7 @@
+const dino = document.querySelector(".dino");
+
+// Adiciona evento para detectar quando a tecla de espaço é pressionada
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+  }
+}
