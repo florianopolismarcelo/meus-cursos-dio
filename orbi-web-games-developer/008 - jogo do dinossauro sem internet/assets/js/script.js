f12da18529e6bf874cbc31426bfd6e2da119783f
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-12-25 09:41:07 -0300

    jogo do dinossauro sem internet - jogo concluído

--- orbi-web-games-developer/008 - jogo do dinossauro sem internet/assets/js/script.js
+++ orbi-web-games-developer/008 - jogo do dinossauro sem internet/assets/js/script.js
@@ -1,25 +1,28 @@
 const dino = document.querySelector(".dino")
+const background = document.querySelector(".background") // Selecionar o container
+
 let isJumping = false
-let position = 0 // É necessário declarar a variável de posição
+let isGameOver = false // Declarar estado inicial do jogo
+let position = 0
 
 // Adiciona evento para detectar quando a tecla de espaço é pressionada
 function handleKeyUp(event) {
-  // event.keyCode está depreciado, use event.code === "Space"
   if (event.code === "Space") {
     if (!isJumping) {
       jump()
     }
   }
 }
+
 // Função para fazer o dinossauro pular
 function jump() {
   isJumping = true
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
-      // Para de subir e começa a descer
       clearInterval(upInterval)
 
+      // Descendo
       let downInterval = setInterval(() => {
         if (position <= 0) {
           clearInterval(downInterval)
@@ -37,5 +40,36 @@ function jump() {
   }, 20)
 }
 
-// Adicione o ouvinte de evento para o código funcionar
+// função cria cactos
+function createCactus() {
+  if (isGameOver) return // Para a criação se o jogo acabou
+
+  const cactus = document.createElement("div")
+  let cactusPosition = 1000
+  let randomTime = Math.random() * 8000
+
+  cactus.classList.add("cactus")
+  background.appendChild(cactus)
+  cactus.style.left = cactusPosition + "px"
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftTimer)
+      background.removeChild(cactus)
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Lógica de Colisão
+      clearInterval(leftTimer)
+      isGameOver = true
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
+    } else {
+      cactusPosition -= 9
+      cactus.style.left = cactusPosition + "px"
+    }
+  }, 20)
+
+  // Gera o próximo cacto recursivamente
+  setTimeout(createCactus, randomTime)
+}
+// Inicia o jogo
+createCactus()
 document.addEventListener("keyup", handleKeyUp)
